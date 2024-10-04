e7c6c42ff69650279a49412528a34a48c1f61903
Artur199324 <artyr241993@gmail.com>
2024-10-04 15:41:19 +0300

    Initial Commit

---
+++ Nova Scotia Resort Places/Nova_Scotia_Resort_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nova_Scotia_Resort_PlacesApp.swift
+//  Nova Scotia Resort Places
+//
+//  Created by Artur on 04.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Nova_Scotia_Resort_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
